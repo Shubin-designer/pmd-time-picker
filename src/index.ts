5443d9bfdf2753dc74d0f3fbed5fed8ddb357fc0
Shubin-designer <115276469+Shubin-designer@users.noreply.github.com>
2025-07-29 01:40:00 +0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
+/// <reference types="@emotion/react/types/css-prop" />
 
+import TimeKeeper from './components/TimeKeeperContainer'
+import type { TimeInput, TimeOutput } from './helpers/types'
+
+export default TimeKeeper
+export type { TimeInput, TimeOutput }
