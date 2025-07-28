8eae83466b43626d038f900890bbd323bfc5a1ed
Shubin-designer <115276469+Shubin-designer@users.noreply.github.com>
2025-07-29 02:46:32 +0300

    Update code.tsx

--- docs /js /components /code.tsx
+++ docs /js /components /code.tsx
@@ -1,1 +1,34 @@
+import React from 'react'
 
+export enum SYNTAX {
+	js = 'language-javascript',
+	css = 'language-scss',
+}
+
+interface Props extends React.HTMLAttributes<HTMLElement> {
+	inline?: boolean
+	type?: SYNTAX
+	children: React.ReactNode
+}
+
+export default function Code({
+	inline = false,
+	children,
+	type = SYNTAX.js,
+	className = '',
+	...rest
+}: Props) {
+	if (inline) {
+		return (
+			<code className={className} {...rest}>
+				{children}
+			</code>
+		)
+	}
+
+	return (
+		<pre className={className} {...rest}>
+			<code className={type}>{children}</code>
+		</pre>
+	)
+}
