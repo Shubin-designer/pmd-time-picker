943b42dffa88e92fab2cbd94df23de0975de38f2
Shubin-designer <115276469+Shubin-designer@users.noreply.github.com>
2025-07-29 02:01:21 +0300

    Create index.tsx

---
+++ src/components /index.tsx
@@ -0,0 +1,7 @@
+/// <reference types="@emotion/react/types/css-prop" />
+import TimeKeeper from './TimeKeeperContainer'
+
+export default TimeKeeper
+
+// при необходимости экспортируем вспомогательные типы
+export type { TimeInput, TimeOutput } from './helpers/types'
