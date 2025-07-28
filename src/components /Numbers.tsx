ecbc3afe4d8849b0b087308638f2bc8b1895f1de
Shubin-designer <115276469+Shubin-designer@users.noreply.github.com>
2025-07-29 02:07:06 +0300

    Create Numbers.tsx

---
+++ src/components /Numbers.tsx
@@ -0,0 +1,147 @@
+import { memo, useMemo } from 'react'
+import { animated, SpringValue } from 'react-spring'
+import DisabledTimeRange from '../helpers/disable-time'
+import { MINUTES, CLOCK_VALUES, MODE, MERIDIEM } from '../helpers/constants'
+import { transform } from '../helpers/math'
+import { numbersStyle, numbersWrapperStyle } from './styles/numbers'
+
+interface CommonProps {
+	disabledTimeRangeValidator: DisabledTimeRange | null
+	anim: {
+		opacity: SpringValue<number>
+		translate: SpringValue<number>
+		translateInner: SpringValue<number>
+	}
+}
+
+interface MinuteProps extends CommonProps {
+	hour: number
+}
+
+interface HourProps extends CommonProps {
+	mode: MODE.HOURS_12 | MODE.HOURS_24
+	hour24Mode: boolean
+	meridiem: MERIDIEM
+}
+
+function Hours({
+	anim,
+	mode,
+	hour24Mode,
+	disabledTimeRangeValidator,
+	meridiem,
+}: HourProps) {
+	const { opacity, translate: translateOuter, translateInner } = anim
+
+	const { numbersOuter, numbersInner } = useMemo(() => {
+		const { numbers: outer, numbersInner: inner } = CLOCK_VALUES[mode]
+		let normalizeOuterIndex: (i: number) => number
+
+		if (mode === MODE.HOURS_12 && meridiem === 'am') {
+			normalizeOuterIndex = i => (i % 11) + 1
+		} else if (mode === MODE.HOURS_12 && meridiem === 'pm') {
+			normalizeOuterIndex = i => (i % 11) + 13
+		} else {
+			normalizeOuterIndex = i => (i % 12) + 13
+		}
+
+		const normalizeInnerIndex = (i: number) => (i % 12) + 1
+
+		return {
+			numbersOuter: outer.map((value, i) => ({
+				value,
+				enabled:
+					disabledTimeRangeValidator?.validateHour(normalizeOuterIndex(i)) ?? true,
+			})),
+			numbersInner: inner?.map((value, i) => ({
+				value,
+				enabled:
+					disabledTimeRangeValidator?.validateHour(normalizeInnerIndex(i)) ?? true,
+			})),
+		}
+	}, [mode, meridiem, disabledTimeRangeValidator])
+
+	return (
+		<animated.div
+			style={{ opacity }}
+			css={numbersWrapperStyle}
+			className="react-timekeeper__clock-hours"
+		>
+			{numbersOuter.map(({ value, enabled }, i) => (
+				<animated.span
+					css={numbersStyle({ hour24Mode, enabled })}
+					key={value}
+					data-testid="number_hour_outer"
+					style={{
+						transform: translateOuter.to(v => transform(i + 1, v)),
+					}}
+				>
+					{value}
+				</animated.span>
+			))}
+
+			{hour24Mode &&
+				numbersInner!.map(({ value, enabled }, i) => (
+					<animated.span
+						css={numbersStyle({ hour24Mode, inner: true, enabled })}
+						key={value}
+						data-testid="number_hour_inner"
+						style={{
+							transform: translateInner.to(v => transform(i + 1, v)),
+						}}
+					>
+						{value}
+					</animated.span>
+				))}
+		</animated.div>
+	)
+}
+
+export const HourNumbers = memo(Hours, (prev, next) => {
+	return (
+		prev.mode === next.mode &&
+		prev.hour24Mode === next.hour24Mode &&
+		prev.meridiem === next.meridiem &&
+		prev.disabledTimeRangeValidator === next.disabledTimeRangeValidator
+	)
+})
+
+function Minutes({ anim, hour, disabledTimeRangeValidator }: MinuteProps) {
+	const { opacity, translate } = anim
+
+	const minutes = useMemo(() => {
+		return MINUTES.map(value => ({
+			value,
+			enabled:
+				disabledTimeRangeValidator?.validateMinute(hour, parseInt(value, 10)) ?? true,
+		}))
+	}, [disabledTimeRangeValidator, hour])
+
+	return (
+		<animated.div
+			style={{ opacity }}
+			css={numbersWrapperStyle}
+			className="react-timekeeper__clock-minutes"
+		>
+			{minutes.map(({ value, enabled }, i) => (
+				<animated.span
+					css={numbersStyle({ enabled })}
+					key={value}
+					data-testid="number_minute"
+					style={{
+						transform: translate.to(v => transform(i + 1, v)),
+					}}
+				>
+					{value}
+				</animated.span>
+			))}
+		</animated.div>
+	)
+}
+
+export const MinuteNumbers = memo(Minutes, (prev, next) => {
+	return (
+		prev.disabledTimeRangeValidator === next.disabledTimeRangeValidator &&
+		prev.hour === next.hour
+	)
+})
